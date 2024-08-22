70ab8f42d2313913efb03e88bf61747732f23405
Indra Puji Novirwan <indrapuji@gmail.com>
2024-08-22 13:24:14 +0700

    Initial Commit

---
+++ shoppingApp/shoppingAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  shoppingAppApp.swift
+//  shoppingApp
+//
+//  Created by Indra Puji Novirwan on 22/08/24.
+//
+
+import SwiftUI
+
+@main
+struct shoppingAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
